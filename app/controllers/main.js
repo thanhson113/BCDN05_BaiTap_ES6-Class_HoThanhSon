2488f603cf3c3fda55d600b76d4eb5ea49453e77
Son <sonho1230@gmail.com>
2022-02-14 16:27:34 +0700

    chinh sua lan 1

--- app/controllers/main.js
+++ app/controllers/main.js
@@ -2,14 +2,41 @@ import GlassSerVices from "../services/glassServices.js";
 
 let glassSer = new GlassSerVices();
 
+let dataGlasses = [];
+let layDSKinh = () => {
+    glassSer.layDSGlass()
+        .then(result => {
+            dataGlasses = result.data;
+            hienThiDSKinh();
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
+layDSKinh();
+
+let hienThiDSKinh = () => {
+    let content = '';
+    dataGlasses.map(item => {
+        let { id, src, virtualImg, brand, name,color, price, description } = item;
+        content += `
+            <div class="col-4 vglasses__item">
+                <img style="cursor:pointer" onclick="ganKinh('${id}')"  class="img-fluid" src="../../assets/${src}" alt="">
+            </div>
+        `
+    })
+    document.querySelector('#vglassesList').innerHTML = content;
+};
+
 let ganKinh = (idGlass) => {
     let avatar = '';
-    let info = ''
+    let info = '';
+
     dataGlasses.map(item => {
         let { id, src, virtualImg, brand, name,color, price, description } = item;
         if (id === idGlass) {
             avatar += `
-                <img src="../../assets/${virtualImg}" alt="">
+                <img class="vglasses__img" src="../../assets/${virtualImg}" alt="">
                 `
             info += `
                 <h5>
@@ -28,34 +55,15 @@ let ganKinh = (idGlass) => {
 };
 window.ganKinh = ganKinh;
 
-let hienThiDSKinh = () => {
-    let content = '';
-    dataGlasses.map(item => {
-        let { id, src, virtualImg, brand, name,color, price, description } = item;
-        content += `
-            <div class="col-4">
-                <img onclick="ganKinh('${id}')"  class="img-fluid" src="../../assets/${src}" alt="">
-            </div>
-        `
-    })
-    document.querySelector('.vgglasses__row').innerHTML = content;
-};
 
-let dataGlasses = [];
-let layDSKinh = () => {
-    glassSer.layDSGlass()
-        .then(result => {
-            dataGlasses = result.data;
-            hienThiDSKinh();
-        })
-        .catch(err => {
-            console.log(err);
-        })
+let removeGlasses = (boolean) => {
+   if(!boolean){
+        document.querySelector('.vglasses__img').style.display = 'none';
+   }else{
+        document.querySelector('.vglasses__img').style.display = 'block';
+   }
 }
-layDSKinh();
-
-
-
+window.removeGlasses = removeGlasses;
 
 
 
